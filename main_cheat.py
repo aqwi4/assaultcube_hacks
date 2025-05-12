ed0a894a9361d058d7e5cacbb8e2700a45fb634d
aqwi4 <164372896+aqwi4@users.noreply.github.com>
2025-05-12 18:05:10 +0300

    Errors analyze update
    Added try - except construction to main_cheat.py

--- main_cheat.py
+++ main_cheat.py
@@ -1,10 +1,13 @@
 import pymem
-pm = pymem.Pymem("ac_client.exe")
-healthpoints_address = 0x007E4A5C
-ammo_address = 0x007E4AB0
-while True:
-	i_health = int(input())
-	i_ammo = int(input())
-	pm.write_int(healthpoints_address, i_health)
-	pm.write_int(ammo_address, i_ammo)
-	print("Values changed!")
+try:
+	pm = pymem.Pymem("ac_client.exe")
+	healthpoints_address = 0x007E4A5C
+	ammo_address = 0x007E4AB0
+	while True:
+		i_health = int(input())
+		i_ammo = int(input())
+		pm.write_int(healthpoints_address, i_health)
+		pm.write_int(ammo_address, i_ammo)
+		print("Values changed!")
+except Exception as e:
+	print(f"Ошибка {e}")
