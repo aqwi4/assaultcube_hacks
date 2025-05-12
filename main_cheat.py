d22bcee79d0e7f6a9338500dbaca0517f85d075b
aqwi4 <164372896+aqwi4@users.noreply.github.com>
2025-05-12 22:57:23 +0300

    Global Rework, now with offsets and pointers!

--- main_cheat.py
+++ main_cheat.py
@@ -1,12 +1,17 @@
 import pymem
+
 try:
 	pm = pymem.Pymem("ac_client.exe")
-	healthpoints_address = 0x007E4A5C
-	ammo_address = 0x007E4AB0
-	i_health = int(input())
-	i_ammo = int(input())
-	pm.write_int(healthpoints_address, i_health)
-	pm.write_int(ammo_address, i_ammo)
-	print("Values changed!")
+	base_address = pymem.process.module_from_name(pm.process_handle, "ac_client.exe").lpBaseOfDll
+	print(f"Base address is: {hex(base_address)}")
+	base_offsets = [0x17E254, 0x17E360,0x18AC00]
+	final_offset = 0xEC
+	for offset in base_offsets:
+		pointer = pm.read_int(base_address + offset)
+		if pointer:
+			hp_adress = pointer + final_offset
+			value = pm.read_int(hp_adress)
+			if value > 0:
+				print(f"We found hp by address {hex(hp_adress)}")
 except Exception as e:
-	print(f"Ошибка {e}")
+	print(f"Error {e}")
